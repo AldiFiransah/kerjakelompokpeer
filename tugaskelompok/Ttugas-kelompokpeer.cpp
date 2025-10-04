ca55e983460b2f32f291366f9be6741618668294
imelia <kndisimelia@gmail.com>
2025-10-04 15:53:08 -0700

    Update Ttugas-kelompokpeer.cpp

---
+++ tugaskelompok/Ttugas-kelompokpeer.cpp
@@ -0,0 +1,18 @@
+#include <iostrean>
+using namespace std;
+
+void PlaylistGoodvibes () {
+  cout << "Playlist Good Vibes" << endl;
+}
+void PlaylistSedih () {
+  cout << "Paylist Sedih" << endl;
+}
+void PlaylistTenang () {
+  cout << "Playlist Tenang" << endl;
+}
+void PlaylistHobah () {
+  cout << "Playlist Hobah" << endl;
+}
+
+  return 0;
+}
